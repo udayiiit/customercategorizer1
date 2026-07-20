925a9d3cf22ab7ad66c44aad1b3b1ef8c3d0d471
udayiiit <udayktr2006@gmail.com>
2026-07-20 14:29:54 +0530

    new file:   static/css/style.css         new file:   templates/.DS_Store         new file:   templates/customer.html

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="uday",
+    version="0.0.1",
+    author="uday",
+    author_email="uadyktr2006@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
